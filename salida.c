85647bb9f4dd1242c5a77dec0e7f76ac1eb624c5
David-Misael <misaelalcocer1906@gmail.com>
2026-07-07 13:38:20 -0600

    images/pruebas

--- salida.c
+++ salida.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 int main() {
-contador = 0;
-contador5printf("%d\n", contador);
-contador = contador + 1;
-contador5printf("%d\n", "Fin del ciclo");
-printf("%d\n", "Error");
+a = 10;
 return 0;
 }
